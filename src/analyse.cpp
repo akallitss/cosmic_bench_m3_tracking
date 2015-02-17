37300169674cd9fc501e1be6d97ca60e4598fccd
quark <sim.bouteille@laposte.net>
2015-02-17 16:51:30 +0100

    fix fit option

--- src/analyse.cpp
+++ src/analyse.cpp
@@ -487,7 +487,7 @@ void Analyse::Residus_ref(){
 		it->second->cd(1);
 		MM_residus[it->first]->Draw();
 		cout << "resolution : " << endl;
-		MM_residus[it->first]->Fit(offset_fit[it->first],"N");
+		MM_residus[it->first]->Fit(offset_fit[it->first],"R");
 		double total_seen = 0;
 		double total_passed = 0;
 		for(int i=1;i<=nbins_2D;i++){
@@ -511,7 +511,7 @@ void Analyse::Residus_ref(){
 		if(point_nb[it->first]>0) correlation[it->first]->Draw("AP");
 		it->second->cd(4);
 		cout << "angle Z : " << endl;
-		angle_alignment[it->first]->Fit(angle_z_fit[it->first]);
+		angle_alignment[it->first]->Fit(angle_z_fit[it->first],"R");
 		angle_alignment[it->first]->Draw();
 		it->second->cd(5);
 		resVSpos[it->first]->Draw();
