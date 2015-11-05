423692bc5211d26210625462fceb863672d36054
quark <sim.bouteille@laposte.net>
2015-11-05 18:03:34 +0100

    increase readability of tomography.h

--- include/tomography.h
+++ include/tomography.h
@@ -45,34 +45,47 @@ namespace Tomography{
 		Dream,
 		Feminos
 	};
-	const double XY_size = 500;
+
+	//Utility function for these enums
+	elec_type str_to_elec(string str);
+	ostream& operator<<(ostream& os, const det_type& det);
+	ostream& operator<<(ostream& os, const strip_type& strip);
+	ostream& operator<<(ostream& os, const elec_type& elec);
+
+	//Real const
 	const double ADC_max = 4096;
 	const int Nsample = 32;
-	const int SampleMin = 5;
-	const int SampleMax = 21;
-	const double sigma = 4.;
-	const int TOTCut = 3;
 	const int Nchannel = 64;
 	const int Nasic_FEU = 8;
 	const int Nasic_Feminos = 4;
 	const string DreamExt = "fdf";
 	const string FeminosExt = "aqs";
-	extern map<const Tomography::det_type,const Detector* const> Static_Detector;
+
+	//Run dependant
+	const double XY_size = 500;
+	const int SampleMin = 5;
+	const int SampleMax = 21;
+	const double sigma = 4.;
+	const int TOTCut = 3;
 	const double chisquare_threshold = 100;
+
+	//List of dummy Detector to iterate over and/or get static members
+	extern map<const Tomography::det_type,const Detector* const> Static_Detector;
+
+	//Help to handle batch/graphic output
 	extern bool live_graphic_display; // toggle updating of canvas during calculation
 	extern bool is_batch;
+	void save_canvases();
+
+	//Handle Ctrl-C
 	extern bool can_continue;// = true;
 	inline void signal_handler(int s){
 		cout << "\nCaught signal " << s << endl;
 		cout << endl;
 		can_continue = false;
 	}
-	elec_type str_to_elec(string str);
-	ostream& operator<<(ostream& os, const det_type& det);
-	ostream& operator<<(ostream& os, const strip_type& strip);
-	ostream& operator<<(ostream& os, const elec_type& elec);
+
 	//void process_elec_files(ptree config_tree);
-	void save_canvases();
 }
 
 #endif
